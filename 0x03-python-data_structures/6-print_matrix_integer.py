b45ad984875370cebc384b24f9505a26b63260e1
21Kgold <claudiarpa@gmail.com>
2022-05-03 07:15:59 -0700

    update

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -3,7 +3,7 @@ def print_matrix_integer(matrix=[[]]):
     i = 0
     for row in matrix:
         for i in range(len(row)):
-            if (i + 1) % 3 != 0:
+            if (i + 1) % len(row) != 0:
                 print("{} ".format(row[i]), end="")
             else:
                 print("{}".format(row[i]), end="")
