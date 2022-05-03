0a83b5b357726adfaf5458bf2560139584d86dfc
21Kgold <claudiarpa@gmail.com>
2022-05-03 07:01:22 -0700

    update

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -3,9 +3,9 @@ def print_reversed_list_integer(my_list=[]):
 
     # First, check for special case when list is empty
 
-    if my_list is not None:
-
+    if my_list is None or my_list == ():
+        return None
         # i range go from 0 inclusive to len exclusive
-
+    else:
         for i in range(len(my_list)):
             print(my_list[-1 - i])
