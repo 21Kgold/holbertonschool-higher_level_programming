85be0763ed1a0960ad6fcd5cdd5b2b6c806cdaea
21Kgold <claudiarpa@gmail.com>
2022-05-03 07:58:34 -0700

    update

---
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+# Function that finds all multiples of 2 in a list
+
+def divisible_by_2(my_list=[]):
+    new = list(my_list)
+    # Check if the list is a special case of empy list
+    if len(my_list) == 0:
+        my_list = None
+    else:
+        for i in range(len(my_list) - 1):
+            if my_list[i] == 0:
+                new[i] = True
+            elif my_list[i] % 2 == 0:
+                new[i] = True
+            else:
+                new[i] = False
+        return new
