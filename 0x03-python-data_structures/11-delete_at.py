4207b2f28312cb1ad298aa290cd123d7b0ce8003
21Kgold <claudiarpa@gmail.com>
2022-05-03 09:38:48 -0700

    update

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -4,7 +4,7 @@
 def delete_at(my_list=[], idx=0):
     # Check if the element to delete it is within the valid range
 
-    if idx > 0 and idx < len(my_list) - 1:
+    if idx >= 0 and idx <= len(my_list) - 1:
         to_remove = my_list[idx]
         my_list.remove(to_remove)
     return my_list
