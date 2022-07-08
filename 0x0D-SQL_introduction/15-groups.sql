a228e71f5547b1b11b92dda1bf3806cf19c94791
21Kgold <claudiarpa@gmail.com>
2022-07-07 18:13:22 -0700

    update

--- 0x0D-SQL_introduction/15-groups.sql
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -2,6 +2,6 @@
 -- The result should display: the score, the number of records for this score with the label number
 -- The list should be sorted by the number of records (descending)
 -- The database name will be passed as an argument to the mysql command
-SELECT score, COUNT(score) AS number
+SELECT score, COUNT(*) AS number
 FROM second_table
 GROUP BY score ORDER BY number;
