854c7f3e33a854407a92dcd6fcc730826b04456d
21Kgold <claudiarpa@gmail.com>
2022-08-01 23:29:17 -0700

    update

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 const message2 = 'Missing size';
-if (!process.argv[2]) {
+if (!process.argv[2] || process.argv[2] < 0 || !parseInt(process.argv[2])) {
   console.log(message2);
 } else if (parseInt(process.argv[2]) && process.argv[2] > 0) {
   for (let i = 0; i < parseInt(process.argv[2]); i++) {
