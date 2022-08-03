6060e31ff658721a23a0767325fce200d637837f
21Kgold <claudiarpa@gmail.com>
2022-08-03 15:37:17 -0700

    update

---
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,29 @@
+#!/usr/bin/node
+module.exports = class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    let square = '';
+    for (let i = 0; i < this.width; i++) {
+      square += 'X';
+    } for (let i = 0; i < this.height; i++) {
+      console.log(square);
+    }
+  }
+
+  rotate () {
+    const temp = this.width;
+    this.width = this.height;
+    this.height = temp;
+  }
+
+  double () {
+    this.width = 2 * this.width;
+    this.height = 2 * this.height;
+  }
+};
