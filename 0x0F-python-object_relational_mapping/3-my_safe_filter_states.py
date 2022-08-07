7b005f22fe8085a08983df4386dcffcdb9106f59
21Kgold <claudiarpa@gmail.com>
2022-08-06 20:00:02 -0700

    update script description

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -3,6 +3,13 @@
 Connect to MySQL database remotely using python’s MySQLdb module,
 lists all states who's name match a command line argument and
 safe from MySQL injections!
+Intruders can cause major harm by performing Python SQL injection by
+accesing databases executing queries, exploting code vulnerabilities,
+Examples of string interpolation that should not be used ever:
+"WHERE name LIKE BINARY '{}'".format(match)
+f"WHERE name LIKE BINARY '{match}'"
+"WHERE name LIKE BINARY '%s'"% match
+WHERE name LIKE BINARY '" + match + "'"
 """
 
 import MySQLdb
@@ -11,11 +18,10 @@ import sys
 if __name__ == "__main__":
     connection = MySQLdb.connect(host="localhost", user=sys.argv[1],
                                  passwd=sys.argv[2], db=sys.argv[3], port=3306)
-    match = str(sys.argv[4])
+    match = sys.argv[4]
     cursor = connection.cursor()
-    select_query = "SELECT * FROM states WHERE name LIKE BINARY '{}' \
-                    ORDER BY id ASC".format(match)
-    cursor.execute(select_query)
+    cursor.execute("SELECT * FROM states WHERE name LIKE BINARY %s \
+                    ORDER BY id ASC", (match, ))
     for row in cursor.fetchall():
         print(row)
     connection.close()
