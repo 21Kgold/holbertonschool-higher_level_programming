2ffb38b0b49424b27acedb3e6288b76f7b5cb720
21Kgold <claudiarpa@gmail.com>
2022-08-19 12:14:17 -0700

    update

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -11,9 +11,3 @@ if __name__ == "__main__":
         body = response.read()
     print(f'Body response:\n\t- type: {body.__class__}\n\t- content: {body}\
 \n\t- utf8 content: {body.decode(encoding="UTF-8",errors="strict")}')
-
-"""
-The Request() object specifies the URL to fetch and represents the
-HTTP request to be made; it's created Calling urlopen(). This response
-is a file-like object, which means you can call .read() on the response
-"""
