5088677bbcf08cf924f72b292a363e99957285bd
21Kgold <claudiarpa@gmail.com>
2022-08-18 17:46:02 -0700

    update

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -3,4 +3,4 @@
 # curl -s do not show progress bar
 # cut -d parameter to set delimiter
 # cut -f to select portion to keep, in this case second portion
-curl -sI $1 | grep 'Content-Length' | cut -d ' ' -f 2
+curl -sI '$1' | grep 'Content-Length' | cut -d ' ' -f 2
