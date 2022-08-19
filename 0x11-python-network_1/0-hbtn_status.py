6e0749f0bc49e57249b5dc3a558a1f16912b1fcf
21Kgold <claudiarpa@gmail.com>
2022-08-19 12:08:09 -0700

    update

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -4,13 +4,13 @@ Script that fetches a url using the "urllib" package and the "with" statement
 """
 
 import urllib.request
-from pprint import pprint
 
-url = 'https://intranet.hbtn.io/status'
-with urllib.request.urlopen(url) as response:
-    Body = response.read()
-print(f'Body response:\n\t- type: {Body.__class__}\n\t- content: \
-{Body}\n\t- utf8 content: {Body.decode(encoding="UTF-8",errors="strict")}')
+if __name__ == "__main__":
+    url = 'https://intranet.hbtn.io/status'
+    with urllib.request.urlopen(url) as response:
+        body = response.read()
+    print(f'Body response:\n\t- type: {body.__class__}\n\t- content: {body}\
+\n\t- utf8 content: {body.decode(encoding="UTF-8",errors="strict")}')
 
 """
 The Request() object specifies the URL to fetch and represents the
