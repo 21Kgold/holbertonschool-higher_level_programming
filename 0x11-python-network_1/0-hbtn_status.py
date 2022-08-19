65cc89907a7e57a0ff4884a901bcf03d51571c8d
21Kgold <claudiarpa@gmail.com>
2022-08-19 12:26:32 -0700

    update

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -7,7 +7,8 @@ import urllib.request
 
 if __name__ == "__main__":
     url = 'https://intranet.hbtn.io/status'
-    with urllib.request.urlopen(url) as response:
+    req = urllib.request.Request(url)
+    with urllib.request.urlopen(req) as response:
         body = response.read()
     print(f'Body response:\n\t- type: {body.__class__}\n\t- content: {body}\
 \n\t- utf8 content: {body.decode()}')
