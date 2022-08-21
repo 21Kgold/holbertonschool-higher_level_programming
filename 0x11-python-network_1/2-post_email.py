9c76f77c7269d3470bf8c3751ca087becdbcf781
21Kgold <claudiarpa@gmail.com>
2022-08-20 23:34:05 -0700

    update

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -8,7 +8,7 @@ import urllib.request
 import urllib.parse
 import sys
 
-if __name__ == "__main__": 
+if __name__ == "__main__":
     url = sys.argv[1]
     # Create a dictionary to store the key-value pairs of POST parameters
     params_dict = {
