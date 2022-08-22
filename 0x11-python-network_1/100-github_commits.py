92d2f131c96f68c09f98ff2e3cda0f4737374f54
21Kgold <claudiarpa@gmail.com>
2022-08-21 20:02:44 -0700

    update

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 """
-Please list 10 commits (from the most recent to oldest) of the repository “rails” by the user “rails”
-You must use the GitHub API, here is the documentation https://developer.github.com/v3/repos/commits/
-Print all commits by: `<sha>: <author name>` (one by line)
+Please list 10 commits (from the most recent to oldest) of the repository
+“rails” by the user “rails” You must use the GitHub API, here is the
+documentation https://developer.github.com/v3/repos/commits/ Print all
+commits by: `<sha>: <author name>` (one by line)
 """
 
 import requests
@@ -11,7 +12,8 @@ import sys
 if __name__ == "__main__":
     repository_name = sys.argv[1]
     owner_name = sys.argv[2]
-    url = "https://api.github.com/repos/" + user + "/" + repo + "/commits"
+    url = "https://api.github.com/repos/" + owner_name + "/" +\
+        repository_name + "/commits"
     response = requests.get(url)
     json_response = response.json()
     new_list = []
