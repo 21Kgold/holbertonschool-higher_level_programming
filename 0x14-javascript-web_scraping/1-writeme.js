3435bc5497bcff4164dc293c713ac94e621c05ea
21Kgold <claudiarpa@gmail.com>
2022-09-13 12:42:05 -0700

    update

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -2,7 +2,7 @@
 // Script that writes a string to a file
 
 const fs = require('fs');
-fs.writeFile(process.argv[2], 'Python is cool', (err, data) => {
+fs.writeFile(process.argv[2], process.argv[3], (err, data) => {
   if (err) {
     console.error(err);
   }
