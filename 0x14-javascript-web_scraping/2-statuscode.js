1567fcb28ba8842a2f5569bd15065f96aef0088d
21Kgold <claudiarpa@gmail.com>
2022-09-13 14:11:09 -0700

    update

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -1,12 +1,12 @@
 #!/usr/bin/node
-// Script that display the status code of a GET request
+// Script that display the status code of a HTTP Get request
 
 const axios = require('axios');
 axios.get(process.argv[2])
   .then((res) => {
-    console.log(`Code: ${res.status}`);
+    console.log(`code: ${res.status}`);
   }).catch(function (error) {
     if (error.response) {
-      console.log('Code:', error.response.status);
+      console.log('code:', error.response.status);
     }
   });
