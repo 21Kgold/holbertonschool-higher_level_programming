ca8e364d4833b8c3fe92c60a57fffebd99b28920
21Kgold <claudiarpa@gmail.com>
2022-09-14 00:46:38 -0700

    update

---
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -0,0 +1,19 @@
+#!/usr/bin/node
+// Script that prints all characters of a Star Wars movie:
+// The first argument is the Movie ID - example: 3 = “Return of the Jedi”
+// Display one character name by line
+// Use the starwars Api https://swapi-api.hbtn.io/
+
+const axios = require('axios');
+const URL = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
+axios.get(URL)
+  .then(function (response) {
+    const films = response.data.characters;
+    for (let i = 0; i < films.length; i++) {
+      axios.get(films[i])
+        .then(function (res) {
+          const name = res.data.name;
+          console.log(name);
+        });
+    }
+  });
