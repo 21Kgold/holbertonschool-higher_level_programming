dd077bbfa8d2d33acc737d12a7d8c214c947a926
21Kgold <claudiarpa@gmail.com>
2022-09-21 17:11:13 -0700

    first commit

---
+++ 0x15-javascript-web_jquery/0-script.js
@@ -0,0 +1,5 @@
+/* The document object represents the whole html document
+querySelector () method finds the first element that matches with any of the
+specified CSS selectors.
+sintax: document.querySelector('tag_name').style.property = 'new_style'; */
+document.querySelector('header').style.color = '#FF0000';
